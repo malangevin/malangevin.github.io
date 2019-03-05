177781bd0834c5c0ab5495f8f0a2541d7085fd9b
Marc-André Brunelle-Langevin <malangevin@dialoginsight.com>
2019-03-05 14:49:28 -0500

    Ajout nouvelle fonction

--- DI.WebPush-sw.js
+++ DI.WebPush-sw.js
@@ -1,1 +1,1 @@
-importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/19329/11854/r5H2E1rnsLb7qK7lZwEp3XxbEL9geRWb/DI.WebPush-sw-lib.js');
+importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/21331/11856/COAi5y099rmucIqlIsUzRiDpiK0y2skW/DI.WebPush-sw-lib.js');
