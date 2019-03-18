fbd326286e158a9b3488417abaa1335b85154844
malangevin <48253797+malangevin@users.noreply.github.com>
2019-03-18 16:09:58 -0400

    Rename DI.WebPush-sw (20).js to DI.WebPush-sw.js

--- DI.WebPush-sw (20).js
+++
@@ -1,1 +0,0 @@
-importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/21331/12457/w3Wlvvjg922ikQRRkarmCjaVlCbJpqcM/DI.WebPush-sw-lib.js');
---
+++ DI.WebPush-sw.js
@@ -0,0 +1,1 @@
+importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/21331/12457/w3Wlvvjg922ikQRRkarmCjaVlCbJpqcM/DI.WebPush-sw-lib.js');
