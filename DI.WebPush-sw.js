8de7b80636d59332c4de35850938068803e58961
malangevin <48253797+malangevin@users.noreply.github.com>
2019-03-26 14:37:47 -0400

    Update DI.WebPush-sw.js

--- DI.WebPush-sw.js
+++ DI.WebPush-sw.js
@@ -1,1 +1,1 @@
-importScripts("http://repo.dev-mabl.hq2.rep/T/OFC4/WPS/21331/12457/w3Wlvvjg922ikQRRkarmCjaVlCbJpqcM/DI.WebPush-sw-lib.js"></script>
+importScripts("http://dipush.devapp.hq2.rep/T/OFC4/WPS/21331/12457/w3Wlvvjg922ikQRRkarmCjaVlCbJpqcM/DI.WebPush-sw-lib.js"></script>
