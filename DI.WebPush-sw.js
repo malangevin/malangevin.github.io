23360166bfad9f6dd73bc47e2384e03871cd76fd
malangevin <48253797+malangevin@users.noreply.github.com>
2019-03-27 16:43:39 -0400

    Update DI.WebPush-sw.js

--- DI.WebPush-sw.js
+++ DI.WebPush-sw.js
@@ -1,1 +1,1 @@
-importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/21331/12457/w3Wlvvjg922ikQRRkarmCjaVlCbJpqcM/DI.WebPush-sw-lib.js');
+importScripts('http://infofcm.devapp.hq2.rep/T/OFC4/WPS/21331/12457/w3Wlvvjg922ikQRRkarmCjaVlCbJpqcM/DI.WebPush-sw-lib.js');
